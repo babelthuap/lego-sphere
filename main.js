875a01f64170df3360e4075c45b2a7748ee04112
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2024-01-11 15:52:16 -0800

    Use more sub-voxel samples

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const d = 150;
-const r = d / 2;
-const r2 = r * r;
-
 const voxelWidth = 5;
 const voxelHeight = 3;
 
-function draw(layer) {
+function draw() {
+  console.time('draw');
+
+  const d = (+diameterInput.value) * voxelWidth;
+  const r = d / 2;
+  const r2 = r * r;
+
+  const layer = +layerInput.value;
   const z = -r + (layer + 0.5) * voxelHeight;
 
   grid.innerHTML = '';
@@ -15,32 +18,39 @@ function draw(layer) {
     for (let col = 0; col < d / voxelWidth; col++) {
       const x = -r + (col + 0.5) * voxelWidth;
       const td = document.createElement('td');
-      td.style.backgroundColor = shade(x, y, z);
+      td.style.backgroundColor = shade(x, y, z, r2);
       tr.appendChild(td);
     }
     grid.appendChild(tr);
   }
+
+  console.timeEnd('draw');
 }
 
-const offsets = [-1 / 2, 1 / 2];
-function shade(x, y, z) {
-  let hits = 0;
+const offsets = [-1 / 2, -3 / 8, -1 / 4, -1 / 8, 0, 1 / 8, 1 / 4, 3 / 8, 1 / 2];
+function shade(x, y, z, r2) {
+  let count = 0;
   for (let dx of offsets) {
+    dx *= voxelWidth;
     for (let dy of offsets) {
+      dy *= voxelWidth;
       for (let dz of offsets) {
-        if ((x + dx) ** 2 + (y + dy) ** 2 + (z + dz) ** 2 < r2) {
-          continue;
-        } else {
-          return 'white';
+        dz *= voxelHeight;
+        if ((x + dx) ** 2 + (y + dy) ** 2 + (z + dz) ** 2 <= r2) {
+          count++;
+          if (count > (offsets.length ** 3) / 2) {
+            return 'black';
+          }
         }
       }
     }
   }
-  return 'black';
+  return 'white';
 }
 
 
-level.addEventListener('input', () => draw(+level.value));
+diameterInput.addEventListener('input', draw);
+layerInput.addEventListener('input', draw);
 
-draw(+level.value);
-level.focus();
+draw();
+layerInput.focus();
